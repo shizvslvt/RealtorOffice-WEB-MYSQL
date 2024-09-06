9c5bb9dba6c671a4a2e7c17ed868a6548a9acf95
shizvslvt <shizvslvt@gmail.com>
2024-09-06 17:29:31 +0300

    eliminated the need to create a user in the database edited tweaked the display of the property name

--- data/db.php
+++ data/db.php
@@ -2,9 +2,10 @@
 global $db;
 
 $servername = "localhost";
-$username = "admin";
-$password = "admin";
+$username = "root";
+$password = "";
 $dbname = "realtor_office";
-$db = mysqli_connect($servername, $username, $password, $dbname);
 
+$db = mysqli_connect($servername, $username, $password, $dbname);
 if(!$db) die("Connection Filed". mysqli_connect_error());
+$db->set_charset("utf8");
